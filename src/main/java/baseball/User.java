71e455121861aea3438ce3fa63bfad6ad8752724
shinjeongbin <shinbin6990@gmail.com>
2022-07-18 11:21:26 +0900

    User.java update

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 
 public class User {
     ArrayList<Integer> numbers = new ArrayList<>();
+    UserInput userInput = new UserInput();
 
-
+    void myInput() {
+        numbers = userInput.input();
+    }
+    ArrayList<Integer> getNumbers() {
+        return this.numbers;
+    }
 }
