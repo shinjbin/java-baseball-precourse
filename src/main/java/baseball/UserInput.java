a564cb08e6e10a5f8d5320fc748a35f7f1e5ff58
ShinJeongbin <shinbin6990@gmail.com>
2022-07-18 22:15:01 +0900

    user input exception

--- src/main/java/baseball/UserInput.java
+++ src/main/java/baseball/UserInput.java
@@ -2,15 +2,20 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import javax.jws.soap.SOAPBinding;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UserInput {
     ArrayList<Integer> result = new ArrayList<>();
+    UserInputException userInputException = new UserInputException();
 
     ArrayList<Integer> input() {
         result.clear();
         String inputstring = Console.readLine();
+
+
+        userInputException.sample(inputstring);
 
         String[] strArray = inputstring.split("");
         for (String s : strArray) result.add(Integer.parseInt(s));
